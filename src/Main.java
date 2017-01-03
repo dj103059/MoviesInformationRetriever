a8d2c53d649087096395a35629810143b2ce11a4
dj103059 <dj103059@etu-unice.fr>
2017-01-03 13:45:42 +0100

    [Add] Project Structure

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+import fr.inria.edelweiss.kgraph.core.Graph;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        Graph graph  = Graph.create();
+    }
+}
