ccb4b39c0f5e3e04ca5b69adfb7c327b6167996f
maconnicolas <maconnicolas@yahoo.fr>
2017-01-03 14:35:11 +0100

    test commit

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ import fr.inria.edelweiss.kgraph.core.Graph;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello Master!");
         Graph graph  = Graph.create();
     }
 }
