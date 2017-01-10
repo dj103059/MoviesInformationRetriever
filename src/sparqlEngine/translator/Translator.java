ccc158948b433b15b18b6012ca5bc4f45bccf668
dj103059 <dj103059@etu-unice.fr>
2017-01-10 09:57:27 +0100

    [Update] add the translator method (StoredComponent to natural language)

--- src/sparqlEngine/translator/Translator.java
+++ src/sparqlEngine/translator/Translator.java
@@ -1,7 +1,25 @@
 package sparqlEngine.translator;
 
+import init.StoredComponent;
+
 /**
+ * class to translate from owl to natural language
  * Created by titanium on 10/01/2017.
  */
 public class Translator {
+    private static String askMovie = "Your film ";
+
+    public Translator(){
+
+    }
+
+    /**
+     * use the stored component to create the question
+     * @param storedComponent
+     * @return
+     */
+    public String translate(StoredComponent storedComponent){
+        //TODO: use the stored component to create the question
+        return "none";
+    }
 }
