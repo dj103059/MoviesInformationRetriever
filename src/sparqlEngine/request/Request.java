857de0b73f905286042995dd1a966d4d50529106
dj103059 <dj103059@etu-unice.fr>
2017-01-15 13:04:29 +0100

    [Update] modification of the variables, method addFilter realised and new method removeendQuery

--- src/sparqlEngine/request/Request.java
+++ src/sparqlEngine/request/Request.java
@@ -8,11 +8,15 @@ import sparqlEngine.filter.Filter;
  * Created by titanium on 10/01/2017.
  */
 public class Request {
-    private String mainQuery = "Select distinct ?label ?description where {";
-    private String endQuery = "}";
+
+
+    private String prefixeMovie = "prefix mo: <http://www.semanticweb.org/titanium/ontologies/2017/0/untitled-ontology-11#>\n";
+    private String mainQuery = "Select distinct ?label ?comment  where { \n"+"?uri rdfs:label ?label.\n" +
+            "  ?uri rdfs:comment ?comment.\n"+"}";
+
 
     public Request(){
-
+        setMainQuery(prefixeMovie+mainQuery);
     }
 
     /**
@@ -26,13 +30,27 @@ public class Request {
     }
 
     /**
-     *  add filter to a querry
-     * @param querry
+     *  add filter to main querry
      * @param filter
      * @return
      */
-    private String addFilter(String querry, Filter filter){
-        //TODO: return querry with filer
-        return "NON";
+    private void addFilter( Filter filter){
+        removeEndQuery();
+        setMainQuery(getMainQuery()+filter.getFilter()+"}");
+    }
+
+    /**
+     * Remove the endQuerry to put another line
+     */
+    private void removeEndQuery(){
+        setMainQuery(getMainQuery().substring(0,getMainQuery().length()-1));
+    }
+
+    public String getMainQuery() {
+        return mainQuery;
+    }
+
+    public void setMainQuery(String mainQuery) {
+        this.mainQuery = mainQuery;
     }
 }
